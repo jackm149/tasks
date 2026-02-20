f832b22120345ccf9c649b544923ca3b24a0e863
JACK MALLETT <JACKMUD@UDEL.EDU>
2026-02-20 11:12:23 -0500

    updated next two functions

--- src/functions.ts
+++ src/functions.ts
@@ -31,7 +31,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -39,7 +39,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.endsWith("?") ? true : false;
 }
 
 /**
