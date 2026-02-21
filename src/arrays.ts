fe403478f7b1d4252114a3d736d14d758cee738e
JACK MALLETT <JACKMUD@UDEL.EDU>
2026-02-21 16:30:03 -0500

    completed up to removeDollars inclusive

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,14 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    if (numbers.length === 0) {
+        return [];
+    }
+
+    if (numbers.length === 0) {
+        return [numbers[0], numbers[0]];
+    }
+    return [numbers[0], numbers[numbers.length - 1]];
 }
 
 /**
@@ -13,7 +20,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripled = numbers.map((number: number): number => number * 3);
+    return tripled;
 }
 
 /**
@@ -21,7 +29,15 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    return numbers.map(function (str: string): number {
+        const num = parseInt(str, 10);
+
+        if (isNaN(num)) {
+            return 0;
+        }
+
+        return num;
+    });
 }
 
 /**
@@ -32,7 +48,15 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    return amounts.map((amount: string): number => {
+        const removeDS = amount.replace("$", "");
+        const num = parseInt(removeDS, 10);
+
+        if (isNaN(num)) {
+            return 0;
+        }
+        return num;
+    });
 };
 
 /**
